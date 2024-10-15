524143b60fd8a52ee4067b8a510cec8496bbf396
Al-Amin Hossain <alaminmain@gmail.com>
2024-10-15 10:58:44 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHangfire(x =>
+{
+    x.UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseSqlServerStorage(builder.Configuration.GetConnectionString("DBConnection"));
+});
+builder.Services.AddHangfireServer();
+
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseHangfireDashboard();
